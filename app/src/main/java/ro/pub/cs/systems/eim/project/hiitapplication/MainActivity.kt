656230730bf257d315603bedf5c9680c029df8a3
CristianVita <cri999sti@gmail.com>
2023-04-01 23:19:40 +0300

    Initial commit

---
+++ app/src/main/java/ro/pub/cs/systems/eim/project/hiitapplication/MainActivity.kt
@@ -0,0 +1,14 @@
+package ro.pub.cs.systems.eim.project.hiitapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+
+}
+
